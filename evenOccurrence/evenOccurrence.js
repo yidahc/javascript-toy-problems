7dda420f9e200daee9e0a0e9685db2014284f129
yidahc <yidah.a.curiel@gmail.com>
2019-05-16 09:37:28 -0500

    evenOccurrence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -11,7 +11,36 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
-  /* START SOLUTION */
-  /* END SOLUTION */
+  let firstEven; // placeholder for answer
+  arr.forEach(element => { // looping through each element in the array
+    occurances = 0; // make an arr for each element, to store its occurances
+    arr.forEach(el => { // compare the other elements in the array to the current
+      if(element === el) { // element you are looping through in line 16
+        occurances++; // push element into occurances to keep track of it
+      }
+    });
+    if (!firstEven) { // if firstEven is still undefined (no even occurance has been found yet)
+      if (occurances % 2 === 0) { // check if occurances for this element (in line 16) is even
+        firstEven = element; // if occurances for this element is even, make it the firstEven and return it
+        return firstEven;
+      }
+    }
+  });
+  if (firstEven) { // if firstEven is defined, return it
+    return firstEven;
+  } else { // else return null
+    return null;
+  }
 };
+
+
+/*
+I- array
+O- 1 element in array
+E - null if no even-occurrence, 
+J - to find the first element in array that appears an even number of times
+E - count number of times the elements occur in an array, and return the first one whose number is even
+// loop through array
+  // create variable to count amount of time each element appears
+  // if amount is even, return that element
+*/
