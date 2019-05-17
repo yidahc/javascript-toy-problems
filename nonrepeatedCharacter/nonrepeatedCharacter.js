f7b103b2a53eb2cc4477d1d3c08bf883e4221fd6
yidahc <yidah.a.curiel@gmail.com>
2019-05-17 09:46:25 -0500

    almost finished nonrepeatedCharacter, missing 1 edge case (capitals)

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,7 +7,37 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
-  /* START SOLUTION */
-  /* END SOLUTION */
+  let edgeCase = "";
+  if (typeof string !== "string") {
+    return edgeCase
+  }
+  for (var i=0; i < string.length; i++) {
+    let count = 0;
+    for (var e=0; e < string.length; e++) {
+      if (string[i] === string[e]) {
+        count = count+1;
+      }
+    }
+  if (count === 1) {
+    return string[i]
+  }
+  }
+  return edgeCase;
 };
+
+
+/*
+I- string
+O- character
+E- not a string, all characters are repeated, capitals
+J- to find the first character in a string that only appears once
+// check if input is string
+// set placeholder to empty string
+// loop through string
+  // for each character start a counter
+    // loop through string again and check if other characters match this character
+      // add to counter if it does 
+  // if counter equals 1, return that character
+// if placeholder is still empty string, return placeholder
+    
+*/
