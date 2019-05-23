89abd6233b2f2a1c54720c826826a4235500e8ab
yidahc <yidah.a.curiel@gmail.com>
2019-05-23 09:44:37 -0500

    finished largest product of three

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -11,10 +11,46 @@
 
 /* END SOLUTION */
 
+var sortNumbers = function (array) {
+  let done = false;
+  while (!done) {
+    done = true;
+    for (var i = 1; i < array.length; i++) {
+      if (array[i-1] > array[i]) {
+        done = false;
+        [array[i], array[i-1]] = [array[i-1], array[i]]
+      }
+    }
+  }
+  return array
+}
+
 var largestProductOfThree = function(array) {
-  /* START SOLUTION */
-  // Make a copy of the input array and sort it numerically
-  /* ELSE
-  // TODO: everything
-  END SOLUTION */
+  sortNumbers(array);
+  return array[array.length-1] * array[array.length-2] * array[array.length-3]
 };
+
+
+/*
+I- array
+O- number
+E- not a number
+J- to find the 3 numbers that multiplied by each other equal to the largest ammount
+// set placeholder array to first index, second and third
+// loop through array, starting at 4th
+  // loop through placeholder array
+    // check if this number (from input array) is larger than any number in placeholder array
+      // if it is, replace that placeholder array number with input array number
+// return placeholder array multiplied by each other
+*/
+
+/*
+// Create a convenience function that sorts arrays ascending numerically
+// loop through input array, starting at 1
+  // check if it is larger than the one before it
+    // if it is not, switch with that element
+// run loop again, until fully sorted (using a while loop to determine when you are done looping)
+
+// use sorting function
+// return the last three numbers multiplied by each other
+*/
--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,14 +12,71 @@
  * Try your function with long, random strings to make sure it handles large
  * inputs well.
  */
-
+//input: string
+//output: first last index of run in array
+//constraints: 0
+//edgecases: return first run if more than one are equal
 var longestRun = function (string) {
-  // TODO: Your code here!
-  /* START SOLUTION */
+  var max = [0,0];
+  var current = [0,0];
+  for (var i = 1; i < string.length; i++){
+    if (string[i-1] === string[i]) {
+      current [1] = i
+      if (current[1] - current[0] > max[1] - max[0]) {
+        max = current
+      }
+    } else {
+      current = [i, i];
+    } 
+  }
+  return max;
+}
   
-  /* END SOLUTION */
-};
+  
+  //check if input is string
+  if(typeof string !== 'string') {
+    return 'Not A String!'
+  }
+  //create result 
+  let result = []; 
+  //create placeholder object for char count
+  let charObj = {};
+  //create counter array [0, character]
+  let count = {
+    A: 0
+  };
+  //iterate string
+  for (let i = 0; i < string.length; i++) {
+  //check if string[i] exists in object
+    if(charObj.hasOwnProperty(string[i])) {
+      //if it does increase count
+      charObj[string[i]]++;
+    } else {
+      //else assign property with a value of 1 
+      charObj[string[i]] = 1;
+    }
+      
 
+      
+  }
+  //iterate object 
+  for (let key in charObj) {
+    //find longest character
+    if(charObj[key] > count['A']){
+      count['A'] = charObj[key]
+      console.log(count)
+    }
+  }
+   
+  //iterate string
+ 
+   //check lastindexOf 
+   
+    //push to array
+
+  //return result array  
+};
+console.log(longestRun('aabbbc'))
 // If you need a random string generator, use this!
 // (you wont need this function for your solution but it may help with testing)
 var randomString = function (len) {
@@ -31,4 +88,4 @@ var randomString = function (len) {
   }
 
   return text;
-};
+; };
