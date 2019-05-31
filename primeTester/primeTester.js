dd69017ea4df3fa2b934298a422c63faa4e39b3f
yidahc <yidah.a.curiel@gmail.com>
2019-05-31 09:36:08 -0500

    finished primeTester

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -5,11 +5,30 @@
  */
 
 var primeTester = function(n) {
-  /* END SOLUTION */
+  let x = 2;
+  while (x < n) {
+    if (n % x === 0) {
+      return false 
+    } else {
+      x ++
+    }
+  }
+  return true
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
  * in a user-specified range (inclusive). If you're not quite sure where to start,
  * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
  * saucy, check out the Sieve of Atkin.)
+ */
+
+
+ /*
+I- number
+O- boolean
+loop from 1 to n
+  divide n by number being looped
+  if there is no remainder, return false
+  else, continue
+return true
  */
