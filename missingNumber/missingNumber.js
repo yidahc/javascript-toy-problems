aba5cef0c6934821e76368f03acdd20770bc6ec2
marcocastillo2506 <marcocastillo2506@gmail.com>
2019-06-04 08:07:08 -0500

    missingNumber

---
+++ missingNumber/missingNumber.js
@@ -0,0 +1,20 @@
+/**
+ * You're given an array every number between 1 and 100,000, except one integer
+ * is missing. The numbers are not guaranteed to be consecutive, either! Write a
+ * function to detect which number is missing.
+ *
+ * Extra credit: Refactor your solution to take O(n) time.
+ */
+
+var findMissingNumber = function(array) {
+  // Your code here
+
+/* START SOLUTION */
+/* END SOLUTION */
+};
+
+/**
+ * Here's a helpful array with every number between 1 and 100000, excluding one
+ * random number.
+ */
+var testArray = _.shuffle(_.range(100000)).slice(1);
